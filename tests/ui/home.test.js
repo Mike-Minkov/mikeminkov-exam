a318fc7bd125cf657f15f2766b248b93c2c03412
Mike <mihail.minkov.minkov@gmail.com>
2023-12-22 12:11:16 +0200

    Updated UI tests with URL

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+    await page.goto('https://mikeminkov-exam.onrender.com');
     const heading = await page.$('p');
     const text = await heading.textContent();
-    expect(text).toContain('Grades: 6');  
+    expect(text).toContain('Grades: 3');  
   });
   
